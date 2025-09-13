0cd1e404c1b0853560ed800b23e9def74fddb6a2
user <user@users-MacBook-Pro.local>
2025-09-13 16:22:21 +0700

    fix: code splitting and add lib

--- index.js
+++ index.js
@@ -1,163 +1,56 @@
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
-const rl = readline.createInterface({ input, output });
-    // TODO: Log the answer in a database
+import {lobby} from "./lib/lobby.js";
 
 
+export let keranjang =[];
+export let historyBlj = [];
 
-
-let keranjang =[]
-let historyBlj = []
-let menu = [
+export let menu = [
     {  
-        makanan: 'mie gacoan',
+        makanan: 'Mie Gacoan',
         harga : 15000
     },
     {
-        makanan: 'mie gacoan lvl 2',
+        makanan: 'Mie Gacoan lvl 2',
         harga : 15000
     },
     {
-        makanan: 'mie gacoan lvl 3',
+        makanan: 'Mie Gacoan lvl 3',
         harga : 15000
     },
     {
-        makanan: 'mie suit',
+        makanan: 'Mie Suit',
         harga : 15000
     },
     {
-        makanan: 'mie suit lvl 2',
+        makanan: 'Mie Suit lvl 2',
         harga : 15000
     },
     {
-        makanan: 'mie suit lvl 3',
+        makanan: 'Mie Suit lvl 3',
         harga : 15000
     },
     {
-        makanan: 'udang rambutan',
+        makanan: 'Udang Rambutan',
         harga : 14000
     },
     {
-        makanan: 'pangsit',
+        makanan: 'Pangsit',
         harga : 14000
     },
     {
-        makanan: 'udang keju',
+        makanan: 'Udang Keju',
         harga : 14000
     },
     {
-        makanan: 'iced tea',
+        makanan: 'Iced Tea',
         harga : 6500
     },
     {
-        makanan: 'air mineral',
+        makanan: 'Air Mineral',
         harga : 6500    
     },
-]
+];
 
-function lobby(){
-    console.log(`
-        1. Menu Makanan
-        2. Keranjang
-        3. Histori
-        4. Exit
-    `)
-    rl.question(' SELAMAT DATANG DI GACOAN DELIVERY `', (answer) => {
-        answer = parseInt(answer)
-        switch(answer){
-            case 1:
-                daftarMenu()
-                break
-            case 2 :
-                prosesKeranjang()
-                break
-            case 3 : 
-                history()
-                break
-            case 4 :
-                rl.close()
-                break
-            default :
-                console.log('Error')
-                lobby()
-        }
-    })
-}
 
-function daftarMenu(){
-    let nmrMenu = 0
-    for(let i=0; menu[i] !== undefined; i++){
-        nmrMenu = i
-        console.log(`${nmrMenu + 1}. ${menu[i].makanan} , ${menu[i].harga}`)
-    }
-    function pilihMenu (a){
-        rl.question('Mau berapa banyak? ', (answer) => {
-            answer = parseInt(answer)
-            let pilihanUsr = menu[a]
-            pilihanUsr = {...pilihanUsr, ...{quantity: answer}, ...{subTotal: pilihanUsr.harga * answer}}
-            keranjang = [
-                ...keranjang,
-                ...[pilihanUsr]
-            ]
-            for(let i = 0; keranjang[i] !== undefined; i++){
-                console.log(`\n${keranjang[i].makanan} \nharga : ${keranjang[i].harga} \nquantity :  ${keranjang[i].quantity} \nSubtotal : ${keranjang[i].subTotal}`)
-            }
-            rl.question('\nmau pesan lagi? (yes/no) ', (answer) => {
-                if (answer === 'yes' ){
-                    daftarMenu()
-                }else {
-                    lobby()
-                }
-            })
-        })
-    }
-        rl.question('\nMau yang mana? (input nomor)', (answer) => {
-        answer = parseInt(answer)
-        if (answer <= nmrMenu && answer >= 1){
-            let choice = answer - 1
-            pilihMenu(choice)
-        }else {
-            console.log('\nData yang anda masukkan tidak sesuai\n')
-            daftarMenu()
-        }
-        
-    })
-}
 
-function prosesKeranjang(){
-    let total = 0
-    for (let i = 0; keranjang[i] !== undefined; i++){
-        total += keranjang[i].subTotal
-        console.log(`\n${keranjang[i].makanan} \nharga : ${keranjang[i].harga} \nquantity :  ${keranjang[i].quantity} \nSubtotal : ${keranjang[i].subTotal} \n`)
-    }
-    console.log(`total : ${total}`)
-    rl.question('Bayar Sekarang? (yes/no)', (answer) => {
-        if(answer === 'yes'){
-            historyBlj = [
-                ...historyBlj,
-                ...keranjang
-            ]
-            keranjang = []
-            rl.question('\nPembayaran berhasil.\nSilahkan tunggu dan selamat menikmati makanan & minuman anda', () => {
-                lobby()
-            })
-        }else{
-            lobby()
-        }
-    })
-}
-
-function history(){
-    for(let i = 0; historyBlj[i] !== undefined; i++){
-        console.log(`\n${historyBlj[i].makanan} \nharga : ${historyBlj[i].harga} \nquantity : ${historyBlj[i].quantity} \nSubtotal : ${historyBlj[i].subTotal} \n  `)
-    }
-    rl.question('Kembali ke lobby (yes/no)', (answer) => {
-    if (answer === 'yes'){
-        lobby()
-    }else {
-        history()
-    }
-    })
-}
-
-lobby()
+lobby();
